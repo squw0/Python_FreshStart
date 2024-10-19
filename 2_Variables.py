a05609516b148cbe2cbb48f59d8e1815e24bdb2c
Piotr Zawadzki <squw2@tlen.pl>
2024-10-19 15:09:23 +0200

    First Commit

---
+++ 2_Variables.py
@@ -0,0 +1,21 @@
+#string 
+first_name = "Jan"
+print(f"Hello {first_name}!!!")
+
+
+#int
+age = 25
+print(f"Youre {age} years old")
+
+
+#float
+price = 10.99
+print(f"It costs {price}")
+
+
+#boolean
+is_student = False #True
+if is_student:
+    print("Youre a student!!!!!")
+else:
+    print("Youre not a student :(")
---
+++ 3_TypeCasting.py
@@ -0,0 +1,22 @@
+name = "Jan Jan"
+age = 25
+gpa = 3.2
+is_student = True
+
+#print(type(name))
+
+#gpa = int(gpa)
+#print(gpa)
+
+#age = float(age)
+#print(age)
+
+age = str(age)
+print(age)
+print(type(age))
+
+age += "1" #251
+print(age)
+
+name = bool(name)
+print(name) #True a jak name="" to jest false
